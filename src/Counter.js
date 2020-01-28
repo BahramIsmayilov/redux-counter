cee99b8da717b332ef15f5369cf4f123ab644c7f
Bahram Ismayilov <bahram.ismayilov@hotmail.com>
2020-01-28 11:20:06 +0400

    add functionally for buttons only with react

--- src/Counter.js
+++ src/Counter.js
@@ -1,20 +1,29 @@
 import React from 'react';
 
 const Counter = () => {
+  const [count, setCount] = React.useState(0);
   return (
     <div className="container">
       <div className="row">
         <h2>Counter</h2>
         <h5>Name</h5>
-        <h1>0</h1>
+        <h1>{count}</h1>
         <div className="container-button">
-          <button id="decrease" className="btn">
+          <button
+            id="decrease"
+            className="btn"
+            onClick={() => setCount(count - 1)}
+          >
             decrease
           </button>
-          <button id="increase" className="btn increase">
+          <button
+            id="increase"
+            className="btn increase"
+            onClick={() => setCount(count + 1)}
+          >
             increase
           </button>
-          <button id="reset" className="btn reset">
+          <button id="reset" className="btn reset" onClick={() => setCount(0)}>
             reset
           </button>
         </div>
